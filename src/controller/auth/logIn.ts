9a9271497d9e595c21dd951ec7a0eaf2740a495f
gopalroy1 <gopalroy5397@gmail.com>
2025-12-05 19:15:09 +0530

    fix setting cookies

--- src/controller/auth/logIn.ts
+++ src/controller/auth/logIn.ts
@@ -28,8 +28,8 @@ export const login = async (req: Request, res: Response) => {
     const token = generateToken(user.id);
       res.cookie("token", token, {
     httpOnly: true,
-    secure: process.env.NODE_ENV === "production",
-    sameSite: "lax",
+    secure: false,//earlier process.env.NODE_ENV === "production"
+    sameSite: "none",//Earliaer lax
     maxAge: 7 * 24 * 60 * 60 * 1000, // 7 days
   });
 
